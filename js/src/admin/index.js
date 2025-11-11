7e0a23167bfa3a181429d1d97b5d8c6fce78c820
Bilgi Lovelace <bilgirei@protonmail.com>
2025-11-11 15:20:37 +0100

    fix fix fix please please please blehett

--- js/src/admin/index.js
+++ js/src/admin/index.js
@@ -1,62 +1,59 @@
-flarum.core.app.initializers.add('bilgi42-flarum-autofollow-tags', () => {
-  // Use registry in Flarum 2.0 (extensionData was removed)
-  const extensionData = flarum.core.app.registry || flarum.core.app.extensionData;
+// Export extend array for Flarum 2.0 Admin extender system
+// Get the Admin extender constructor from compat
+const Admin = flarum.core.compat['common/extenders'].Admin;
 
-  if (extensionData) {
-    extensionData
-      .for('bilgi42-flarum-autofollow-tags')
-      .registerSetting(function () {
-        const tags = flarum.core.app.store.all('tags');
-        const selectedTags = JSON.parse(
-          this.setting('bilgi42-autofollow-tags.tag_ids')() || '[]'
-        );
+export const extend = [
+  new Admin().setting(function () {
+      const tags = flarum.core.app.store.all('tags');
+      const selectedTags = JSON.parse(
+        this.setting('bilgi42-autofollow-tags.tag_ids')() || '[]'
+      );
 
-        return (
-          <div className="Form-group">
-            <label>
-              {flarum.core.app.translator.trans(
-                'bilgi42-autofollow-tags.admin.tags_label',
-                {},
-                'Select tags to auto-follow for new users'
-              )}
-            </label>
-            <div className="helpText">
-              {flarum.core.app.translator.trans(
-                'bilgi42-autofollow-tags.admin.tags_help',
-                {},
-                'New users will automatically follow these tags'
-              )}
-            </div>
-
-            <div className="TagSelection">
-              {tags.map((tag) => {
-                const isSelected = selectedTags.includes(tag.id());
-
-                return (
-                  <label className="checkbox" key={tag.id()}>
-                    <input
-                      type="checkbox"
-                      checked={isSelected}
-                      onchange={(e) => {
-                        let newSelected = [...selectedTags];
-                        if (e.target.checked) {
-                          newSelected.push(tag.id());
-                        } else {
-                          newSelected = newSelected.filter((id) => id !== tag.id());
-                        }
-                        this.setting('bilgi42-autofollow-tags.tag_ids')(
-                          JSON.stringify(newSelected)
-                        );
-                        m.redraw();
-                      }}
-                    />
-                    <span style={{ color: tag.color() }}>{tag.name()}</span>
-                  </label>
-                );
-              })}
-            </div>
+      return (
+        <div className="Form-group">
+          <label>
+            {flarum.core.app.translator.trans(
+              'bilgi42-autofollow-tags.admin.tags_label',
+              {},
+              'Select tags to auto-follow for new users'
+            )}
+          </label>
+          <div className="helpText">
+            {flarum.core.app.translator.trans(
+              'bilgi42-autofollow-tags.admin.tags_help',
+              {},
+              'New users will automatically follow these tags'
+            )}
           </div>
-        );
-      });
-  }
-});
+
+          <div className="TagSelection">
+            {tags.map((tag) => {
+              const isSelected = selectedTags.includes(tag.id());
+
+              return (
+                <label className="checkbox" key={tag.id()}>
+                  <input
+                    type="checkbox"
+                    checked={isSelected}
+                    onchange={(e) => {
+                      let newSelected = [...selectedTags];
+                      if (e.target.checked) {
+                        newSelected.push(tag.id());
+                      } else {
+                        newSelected = newSelected.filter((id) => id !== tag.id());
+                      }
+                      this.setting('bilgi42-autofollow-tags.tag_ids')(
+                        JSON.stringify(newSelected)
+                      );
+                      m.redraw();
+                    }}
+                  />
+                  <span style={{ color: tag.color() }}>{tag.name()}</span>
+                </label>
+              );
+            })}
+          </div>
+        </div>
+      );
+    })
+];
