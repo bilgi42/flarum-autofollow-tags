cadd83cf3f514b6844049f46d4d3df8a2f5f929e
Bilgi Lovelace <bilgirei@protonmail.com>
2025-11-11 15:00:14 +0100

    fix fix fix please please please

---
+++ js/src/admin/extend.js
@@ -0,0 +1,59 @@
+import Extend from 'flarum/common/extenders';
+import app from 'flarum/admin/app';
+
+export default [
+  new Extend.Admin()
+    .setting(function () {
+      const tags = app.store.all('tags');
+      const selectedTags = JSON.parse(
+        this.setting('bilgi42-autofollow-tags.tag_ids')() || '[]'
+      );
+
+      return (
+        <div className="Form-group">
+          <label>
+            {app.translator.trans(
+              'bilgi42-autofollow-tags.admin.tags_label',
+              {},
+              'Select tags to auto-follow for new users'
+            )}
+          </label>
+          <div className="helpText">
+            {app.translator.trans(
+              'bilgi42-autofollow-tags.admin.tags_help',
+              {},
+              'New users will automatically follow these tags'
+            )}
+          </div>
+
+          <div className="TagSelection">
+            {tags.map((tag) => {
+              const isSelected = selectedTags.includes(tag.id());
+
+              return (
+                <label className="checkbox" key={tag.id()}>
+                  <input
+                    type="checkbox"
+                    checked={isSelected}
+                    onchange={(e) => {
+                      let newSelected = [...selectedTags];
+                      if (e.target.checked) {
+                        newSelected.push(tag.id());
+                      } else {
+                        newSelected = newSelected.filter((id) => id !== tag.id());
+                      }
+                      this.setting('bilgi42-autofollow-tags.tag_ids')(
+                        JSON.stringify(newSelected)
+                      );
+                      m.redraw();
+                    }}
+                  />
+                  <span style={{ color: tag.color() }}>{tag.name()}</span>
+                </label>
+              );
+            })}
+          </div>
+        </div>
+      );
+    })
+];
